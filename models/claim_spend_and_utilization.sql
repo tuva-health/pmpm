1b1352dbd3d788f71a54e4f4556e1ee57c30e9b6
bentuvan <114525217+bentuvan@users.noreply.github.com>
2022-12-15 14:55:06 -0700

    Refactor pmpm to claims based on internal tuva data (#3)
    * refactor models in the pmpm package to run off claims instead of encounters. Change paid_amounts column names to spend and delete extra columns based on encounter types that we don't have validated yet.
    
    * change two of the models to views since they aren't the final output table and it doesn't seem to hurt performance too much.
    
    * changed the name of the main output table to pmpm builder since we removed the utilization metrics.
    
    * changed the name of the yml file to models.yml to match the standard.
    
    * add coalesce to replace nulls with zeros where the sums of spend for a patient member month is zero.
    
    * changed claims input layer config value to claims common to match our new standard schema for the claims data model.
    
    * change the config to run the pmpm project on the tuva demo sample database.

---
+++ models/claim_spend_and_utilization.sql
@@ -0,0 +1,50 @@
+{{ config(materialized='view') }}
+
+with medical as 
+(
+    select
+        patient_id
+       ,year(claim_end_date) as year
+       ,lpad(month(claim_end_date),2,0) as month
+       ,concat(year(claim_end_date),lpad(month(claim_end_date),2,0)) AS year_month
+       ,claim_type
+       ,paid_amount
+    from {{ var('medical_claim') }}
+)
+, pharmacy as 
+(
+    select
+        patient_id
+        ,year(to_date(dispensing_date)) as year
+        ,lpad(month(to_date(dispensing_date)),2,0) as month
+        ,concat(year(to_date(dispensing_date)),lpad(month(to_date(dispensing_date)),2,0)) as year_month
+        ,'pharmacy' as claim_type
+        ,paid_amount
+    from {{ var('pharmacy_claim') }}
+)
+
+select 
+    patient_id
+    ,claim_type
+    ,year_month
+    ,count(*) as count_claims
+    ,sum(paid_amount) as spend
+from medical
+group by 
+    patient_id
+    ,claim_type
+    ,year_month
+
+union all
+
+select 
+    patient_id
+    ,claim_type
+    ,year_month
+    ,count(*) as count_claims
+    ,sum(paid_amount) as spend
+from pharmacy
+group by 
+    patient_id
+    ,claim_type
+    ,year_month
